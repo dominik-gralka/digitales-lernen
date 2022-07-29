be460e831588ddc6eaa2d60a7834f3a829fc7839
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2022-07-30 00:53:51 +0200

    Update 2.js

--- pages/kurse/basics/2.js
+++ pages/kurse/basics/2.js
@@ -9,7 +9,7 @@ export default function AufundAbleiten() {
         <ProgressHandler />
         <MultipleChoiceQuiz
             question="Wie lautet die Stammfuktion von x?"
-            options={ ["x", "x^2", "x^3", "x^4"] }
+            options={ ["Option 1", "Option 2", "Option 3", "Option 4"] }
             answer={ 0 }
             image="https://img.freepik.com/premium-photo/one-man-traveler-with-backpack-is-standing-looking-sunset-beautiful-nature-landscape-colorful-sun-sky-koh-tarutao-island-satun-thailand-16-9-wide-screen_536080-1318.jpg?w=1480"
         />
