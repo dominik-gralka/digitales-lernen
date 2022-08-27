225be9820f5aac2acb3bb84e168ab0a6c8241b8e
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2022-08-28 01:44:12 +0200

    Update exportData.js

--- pages/account/exportData.js
+++ pages/account/exportData.js
@@ -67,7 +67,7 @@ export async function getServerSideProps(context) {
     const client = cookies.fromClient;
 
     // Make api call to api/exportData
-    const res = await fetch('/api/exportData?user=' + client, {
+    const res = await fetch(process.env.BASE_URL + '/api/exportData?user=' + client, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
