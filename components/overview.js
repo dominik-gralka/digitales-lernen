f2ffe15158ee6edd136f2d1efd16cf93ecaab8d4
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2022-10-17 23:57:09 +0200

    Update overview.js

--- components/overview.js
+++ components/overview.js
@@ -21,7 +21,7 @@ export const Overview = {
         "sections": 3,
         "preview": "/thumbnails/integrieren.png",
         "enabled": false,
-        "available_from": "17.10.2022"
+        "available_from": "21.10.2022"
     },
     "einleitung": {
         "title": "Einleitung",
@@ -37,6 +37,7 @@ export const Overview = {
         "sections": 0,
         "preview": "/thumbnails/placeholder.png",
         "enabled": false,
+        "available_from": "30.10.2022"
     },
     "zwischen_den_integralen": {
         "title": "Zwischen den Integralen",
